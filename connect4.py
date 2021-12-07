bf606559a3f99a43bee972b06e6b8c1d9cab8e59
Octavio Serrano <octavioserrano@Octavio-Serranos-MacBook-Pro-10.local>
2021-12-06 20:05:37 -0800

    diagonal and horizontal loogic

--- connect4.py
+++ connect4.py
@@ -4,7 +4,7 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def creat_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
@@ -12,7 +12,7 @@ def drop_piece(board, row, col, piece):
 
 # first step to pass board and col as arguments here
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -22,6 +22,35 @@ def get_next_open_row(board, col):
    #  flips it on the x axis so the numbers will fall all the way down
 def print_board(board):
     print(np.flip(board, 0))
+    
+    #manually check all the winning moves
+    #and verify to see if there is a wining combo
+def winning_move(board, piece):
+    #check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+            
+    #vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True  
+            
+    # check positively sloped diagonals 
+    
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    
+    # check negatively sloped diagonals      
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3,ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+    
     
 board = creat_board()
 print_board(board)
@@ -36,6 +65,10 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+            
+            if winning_move(board, 1):
+                print("Player 1 Winss!!! Congrats!!!")
+                game_over = True
         
        
           
@@ -46,6 +79,11 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+            
+            if winning_move(board, 2):
+                print("Player 2 Winss!!! Congrats!!!")
+                game_over = True
+                break
             
     print_board(board)
     
